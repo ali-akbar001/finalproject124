5fb939c00f4b508e81288c8120bc4fedc712c946
ali-akbar001 <155974597+ali-akbar001@users.noreply.github.com>
2024-02-21 22:39:44 -0800

    Add files via upload

---
+++ index.html/Restaurant.js
@@ -0,0 +1,11 @@
+var bar = document.getElementById("bar");
+var barjs = document.querySelector(".barjs");
+bar.addEventListener("click",abcd );
+
+function abcd() {
+console.log(bar);
+barjs.classList.toggle("bar_click");
+
+}
+// const onclose = () => {
+//     barjs.classList.toggle("bar_click");
